cea743de5caee3e17d890f3da395e0be5684d68c
Jose Manuel Beauregard Mendez <jm.beauregard26@gmail.com>
2019-04-02 22:52:37 -0600

    fix fix  again

--- routes/api/timeseries.js
+++ routes/api/timeseries.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const Timeseries = mongoose.model('Timeseries');
 
 router.get('/', auth.optional, (req, res, next) => {
-  return Timeseries.find({}).limit(50)
+  return Timeseries.find({})
     .then(tm => res.json({ data: tm }))
     .catch(err => res.json({ msg: err }).status(404));
 });
