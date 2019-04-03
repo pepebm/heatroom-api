62528ea4ea72ba73783aea81980d9a7c4524c367
Jose Manuel Beauregard Mendez <jm.beauregard26@gmail.com>
2019-04-02 20:56:15 -0600

    better queries

--- routes/api/heatmap.js
+++ routes/api/heatmap.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const Hm05m = mongoose.model('Hm05m');
 
 router.get('/', auth.optional, (req, res, next) => {
-  return Hm05m.find({})
+  return Hm05m.find({}).sort({ outtime: -1 }).limit(1)
     .then(hm => res.json({ data: hm }))
     .catch(err => res.json({ msg: err }).status(404));
 });
